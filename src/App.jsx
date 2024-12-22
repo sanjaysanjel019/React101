60651d290e3e517e2e4e447cb2b70742fd7a62fb
sanjaysanjel019 <sanjay.sanjel018@gmail.com>
2024-12-23 00:48:55 +0545

    added logic to change the name

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
-const App = () => {
-  const [movie,setRating] = useState({
-    name : "Bold Target",
-    rating : 4.5
-  });
 
-  const handleChange = () => {
-    setRating({...movie,rating:8})
+// Create a Movie Object and only update the first name of the movie
+const App = () => {
+  const [movies,setRating] = useState([
+    {
+      id:1,
+      name:"Spider Man",
+      rating: 8.0
+    },
+    {
+      id:2,
+      name:"The Memories of Murder",
+      rating: 10.0
+    },
+    {
+      id:3,
+      name:"The Hereditary",
+      rating: 8.5
+    }
+  ]);
+
+  const changeRating = () => {
+    setRating(movies.map((m)=> (m.id ===1 ? {...m,name:"Supadaddy"}:m))
+  );
   }
 
 
@@ -14,12 +30,23 @@ const App = () => {
 <section>
   <h1>Hello</h1>
   <ul>
-    <h1> Title : {movie.name}</h1>
-    <h1> Rating : {movie.rating}</h1>
- 
-
-    <button onClick={handleChange}>Change Raing</button>
+    {movies.map((m)=>(
+      <li key={m.id}>{m.name} and the rating is {m.rating}</li>
+    ))}
   </ul>
+ 
+    <button onClick={changeRating}>Change rating</button>
+
+    {/* <section>
+      <h1>Hello</h1>
+      <ul>
+        {movies.map((m) => (
+          <li key={m.id}>{m.name} rating is {m.rating}</li>
+        ))}
+        <button onClick={changeRating}>Change ratings</button>
+      </ul>
+    </section> */}
+  
  
 </section> 
 )
